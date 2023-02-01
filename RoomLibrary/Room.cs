0a7840f31bad8865c7997bd3839f423f3f1da8a3
Senya <arseniy-selin-2004@yandex.ru>
2023-02-01 14:48:00 +0300

    Add files via upload

--- RoomLibrary/Room.cs
+++ RoomLibrary/Room.cs
@@ -50,5 +50,67 @@ namespace RoomLibrary
         {
             return RoomArea() / np;
         }
+
+        /// <summary>
+        /// Информация о комнате
+        /// </summary>
+        /// <returns>Возвращает строку</returns>
+        public string Info()
+        {
+            return $"Жилая комната площадью {RoomArea()} кв. м.";
+        }
+    }
+
+    /// <summary>
+    /// Класс "жилая комната"
+    /// </summary>
+    public class LivingRoom : Room
+    {
+        int numWin;
+        
+        public int NumWin
+        {
+            get { return numWin; }
+            set { numWin = value; }
+        }
+
+        /// <summary>
+        /// Информация о комнате
+        /// </summary>
+        /// <returns>Возвращает строку</returns>
+        public string Info()
+        {
+            return $"Жилая комната площадью {RoomArea()} кв. м., с {numWin} окнами.";
+        }
+    }
+
+    public class Office : Room
+    {
+        int numSockets;
+
+        public int NumSockets
+        {
+            get { return numSockets; }
+            set { numSockets = value; }
+        }
+
+        /// <summary>
+        /// Максимально возможное число рабочих мест
+        /// </summary>
+        /// <returns>Число мест</returns>
+        public int NumWorkplaces()
+        {
+            int num = Convert.ToInt32(Math.Truncate(RoomArea() / 4.5));
+            return Math.Min(numSockets, num);
+        }
+
+        /// <summary>
+        /// Информация о офиск
+        /// </summary>
+        /// <returns>Возвращает строку</returns>
+        public string Info()
+        {
+            return $"Офис на {NumWorkplaces()} рабочих мест";
+        }
     }
 }
